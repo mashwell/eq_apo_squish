2b2a389f2823021a6423b74278aa655fa72e4736
mashwell <9461543+mashwell@users.noreply.github.com>
2024-05-07 15:38:40 +0700

    skeleton, i/o done

--- eq_apo_squish.py
+++ eq_apo_squish.py
@@ -2,6 +2,8 @@
 import argparse
 # for path handling (Python 3.4+)
 from pathlib import Path
+# for bailing out
+import sys
 
 # argument handling
 parser = argparse.ArgumentParser()
@@ -14,21 +16,34 @@ args = parser.parse_args()
 
 # path handling
 path_to_txt_file = Path(args.path_to_txt_file).absolute()
-# generate output filename
-squished_filename = Path(args.path_to_txt_file).stem + "_squished_" + str(
-    args.squish_amount_pct) + "_pct.txt"
-# test printing parent dir
 parent_dir = Path(args.path_to_txt_file).absolute().parent
+# generate output filename
+squished_filename = "{}_squished_{}_pct.txt".format(
+    Path(args.path_to_txt_file).stem, args.squish_amount_pct)
+## test printing parent dir
 print(f"parent dir: {parent_dir}")
 print(f"will write to: {parent_dir / squished_filename}")
+
+input_lines = []
 
 # open the file
 try:
     with open(path_to_txt_file, 'r') as f:
-        print(f)
+        input_lines = f.readlines()
+        print(input_lines)
 except Exception as e:
-    print(f"Error: Unable to access file: {str(e)}")
+    print(f"Error: Unable to access input file: {str(e)}")
+    sys.exit(1)
 
 # TODO: check for preamp
 # TODO: do things with the filter gains
 # TODO: save the resulting file
+try:
+    with open(parent_dir / squished_filename, 'w') as f:
+        flat_text = ""
+        for lines in input_lines:
+            flat_text += lines
+        f.write(flat_text)
+except Exception as e:
+    print(f"Error: Unable to write output file: {str(e)}")
+    sys.exit(1)
